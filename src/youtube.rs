b8214e5a12cd1d8e4579becd57e13361b79fbd21
TheGeneralist <180094941+thegeneralist01@users.noreply.github.com>
2025-04-04 18:43:45 +0200

    use all cores + postprocessor args

--- src/youtube.rs
+++ src/youtube.rs
@@ -4,17 +4,19 @@ use anyhow::Result;
 use teloxide::types::UserId;
 
 pub async fn download(url: &str, user_id: UserId) -> Result<bool> {
-    // The command I use:
-    // yt-dlp "https://www.youtube.com/watch?v=$1" -o "$2" --merge-output-format mp4
     let cmd = Command::new("yt-dlp")
         .arg(url)
         .arg("-o")
-        // .arg("%(title).90s.%(ext)s")
         .arg(format!("./downloads/{}.%(ext)s", user_id))
         .arg("--merge-output-format")
         .arg("mp4")
+        .arg("--concurrent-fragments")
+        .arg("12")
+        .arg("--postprocessor-args")
+        .arg("ffmpeg:-c:v libx264 -profile:v high -preset veryfast -b:v 4133k -bufsize 8266k -maxrate 4500k -r 60 -c:a aac -b:a 128k -threads 12")
         .output()
         .expect("Failed to execute command");
+
     log::debug!("yt-dlp output: {:?}", cmd);
     Ok(cmd.status.success())
 }
